9bf24abf8f4daaef4056f134dda78c8265eda6db
wangxiaoniu <13519870949@163.com>
2017-04-18 10:30:08 +0800

    333

--- task2/task2.js
+++ task2/task2.js
@@ -1,59 +1,43 @@
 
-//加载函数
-function addLoadEvent(func){
-	var oldonload=window.onload;
-	if(typeof window.onload!="function"){
-		window.onload=func;
-	}else{
-		window.onload=function(){
-			oldonload();
-			func();
-		}
-	}
-}
-
+var mobile=document.getElementById('mobile');
+var username=document.getElementById('name');
 //手机验证
 function mobileValidate(){
-	var mobile=document.getElementById('mobile');
 	var reg=/^1[3|4|5|8]\d{9}$/g;
-	if(!(reg.test(mobile.value)))
+	if(mobile.value == ""){
+		alert("不能为空!");
+		return false;
+	}
+	else if(!(reg.test(mobile.value)))
 	{
+		alert("手机号格式输入错误，请重新输入！");
 		return false;
 	}else{	
-		return true;
+		alert("正确");
 	}
 
 }
 //验证用户名
 function nameValidate(){
 	var reg=/\b([A-Za-z]{3}\s){2}[A-Za-z]+$/;
-	var name=document.getElementById('name');
-	if(!(reg.test(name.value))){
+	if(username.value == ""){
+		alert("不能为空!");
+		return false;
+	}
+	else if(!(reg.test(username.value))){
+		alert("用户名格式输入错误，请重新输入！");
 		return false;
 	}
 	else{
-		return true;
+		alert("正确");
 	}
 }
 
+mobile.onblur=mobileValidate;
+username.onblur=nameValidate;
 
-//表单提交
-function formValidate(){
-	mobileValidate();
-	nameValidate();
-	if( mobileValidate()!=1){
- 		alert("手机号格式输入错误，请重新输入！");
- 		return false;
- 	}
- 	if( nameValidate()!=1){
- 		alert("用户名格式输入错误，请重新输入！");
- 		return false;
- 	}
- 	if(mobileValidate()=1 && ameValidate()=1){
- 		alert('提交成功');
-	}
-	
-}
+
+
 
 
 
