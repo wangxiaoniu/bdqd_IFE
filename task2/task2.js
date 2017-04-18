be9be134796431140d4c342bf545e3ab21928fdf
wangxiaoniu <13519870949@163.com>
2017-04-18 09:58:19 +0800

    222

--- task2/task2.js
+++ task2/task2.js
@@ -15,27 +15,40 @@ function addLoadEvent(func){
 //手机验证
 function mobileValidate(){
 	var mobile=document.getElementById('mobile');
-	var reg=/^1[3|4|5|8][0-9]\d{8}$/g;
-	if(!(reg.test(mobile.value))){
-		
+	var reg=/^1[3|4|5|8]\d{9}$/g;
+	if(!(reg.test(mobile.value)))
+	{
 		return false;
-	}else{
-		
+	}else{	
 		return true;
 	}
 
 }
+//验证用户名
+function nameValidate(){
+	var reg=/\b([A-Za-z]{3}\s){2}[A-Za-z]+$/;
+	var name=document.getElementById('name');
+	if(!(reg.test(name.value))){
+		return false;
+	}
+	else{
+		return true;
+	}
+}
+
 
 //表单提交
 function formValidate(){
 	mobileValidate();
-
-	if( mobileValidate()!=1 ){
- 		alert("输入有误，请重新检查");
+	nameValidate();
+	if( mobileValidate()!=1){
+ 		alert("手机号格式输入错误，请重新输入！");
+ 		return false;
+ 	}else if( nameValidate()!=1){
+ 		alert("用户名格式输入错误，请重新输入！");
  		return false;
  	}else{
  		alert('提交成功');
-
 	}
 	
 }
